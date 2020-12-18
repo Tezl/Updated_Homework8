50676cbfb9ded79862564dc824b03d46a95d550f
Fistashka <tizilpe@gmail.com>
2020-12-18 20:25:55 +0200

    Initial commit

---
+++ src/homework8_UPD/Main.java
@@ -0,0 +1,11 @@
+package homework8_UPD;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Person.displayAccount);
+
+    }
+
+}
---
+++ src/homework8_UPD/Person.java
@@ -0,0 +1,26 @@
+package homework8_UPD;
+
+public class Person {
+
+    public static boolean displayAccount;
+    public String name;
+
+    public class Account {
+
+        int password;
+
+        public void password() {
+
+            int password = 880055535;
+
+        }
+
+        public void displayAccount() {
+
+            System.out.println("Person name - " + name);
+
+        }
+
+    }
+
+}
